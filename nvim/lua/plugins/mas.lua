87e309376e2e08618b32a74aa1051c52e47ec330
ethanprogramser <168668562+ethanprogramser@users.noreply.github.com>
2024-10-24 19:09:59 -0400

    Add files via upload

---
+++ nvim/lua/plugins/mas.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/noice.nvim",
+  config = function()
+  require("noice").setup({
+        -- add any options here
+        -- routes = {
+        --   {
+        --     view = "notify",
+        --     filter = { event = "msg_showmode" },
+        --   },
+        -- },
+      })
+    end,}
